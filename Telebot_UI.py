c2b567c443a98af5286ac9e2a940492414adf672
prolisbet <lizatrushina96@gmail.com>
2024-04-25 01:49:18 +0300

    correct code
    -Добавила отступы и переносы строк
    -Убрала вызов функции get_all_habits
    -Скорректировала параметры функции  add_habit_to_user_list_directly внутри функции process_habit_frequency_step
    -Переименовала функцию add_new_habit в show_all_habits, чтобы подходило по смыслу
    -Закомментировала ненужную функцию show_all_habits, написанную ранее
    -Внутри функции handle_pagination убрала вызов функции get_all_habits

--- Telebot_UI.py
+++ Telebot_UI.py
@@ -4,20 +4,21 @@ import telebot
 from telebot import types
 # Следующая строка не нужна, так как предыдущая уже импортирует нужные классы объектов types
 # ??? ЗАПРОС НА УДАЛЕНИЕ ???
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+# from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 # Импорт функций из файла Python Logic_Back_end.py
 from Logic_Back_end import (add_or_get_user,
-                            add_habit_to_user_list, get_user_habits, get_new_habits, add_habit_to_user_list_directly, get_all_habits)
+                            add_habit_to_user_list, get_user_habits, get_new_habits,
+                            add_habit_to_user_list_directly)  # get_all_habits
 
 # Ввод токена основного телеграм-бота и инициализация программы:
-# TOKEN = '6795112102:AAFBiEZg3Jgi2XxAoqsJvLzUGfSsmvNempo'
-# bot = telebot.TeleBot(TOKEN)
+TOKEN = '6795112102:AAFBiEZg3Jgi2XxAoqsJvLzUGfSsmvNempo'
+bot = telebot.TeleBot(TOKEN)
 
 # !!! ПЕРЕКЛЮЧИТЬ НА ОСНОВНОЙ ТЕЛЕГРАМ-БОТ В ФИНАЛЬНОЙ ВЕРСИИ ПРОГРАММЫ !!!
 
 # Ввод токена тестового телеграм-бота и инициализация программы
-TEST_TOKEN = '6795112102:AAFBiEZg3Jgi2XxAoqsJvLzUGfSsmvNempo'
-bot = telebot.TeleBot(TEST_TOKEN)
+# TEST_TOKEN = '7088266760:AAG2r0Dz3GJAymtpxqrQpapNgVC91u8E23Q'
+# bot = telebot.TeleBot(TEST_TOKEN)
 
 
 # Функция вызова списка привычек из таблицы 'habits' в виде клавиатуры для выбора пользователя
@@ -63,37 +64,46 @@ def send_welcome(message):
 # @bot.callback_query_handler(func=lambda call: call.data == 'add_new_habit')
 # def show_all_habits_button(call):
 #     show_all_habits(call.message)
+
+
 @bot.callback_query_handler(func=lambda call: call.data == 'add_new_habit')
 def add_new_habit_button(call):
     # Начало диалога для добавления новой привычки
     msg = bot.send_message(call.message.chat.id, "Введите название новой привычки:")
     bot.register_next_step_handler(msg, process_habit_name_step, user_id=call.from_user.id)
 
+
 def process_habit_name_step(message, user_id):
     habit_name = message.text
     msg = bot.send_message(message.chat.id, "Введите описание привычки:")
     bot.register_next_step_handler(msg, process_habit_description_step, user_id=user_id, habit_name=habit_name)
 
+
 def process_habit_description_step(message, user_id, habit_name):
     description = message.text
     msg = bot.send_message(message.chat.id, "Введите цель привычки:")
-    bot.register_next_step_handler(msg, process_habit_goal_step, user_id=user_id, habit_name=habit_name, description=description)
+    bot.register_next_step_handler(msg, process_habit_goal_step, user_id=user_id, habit_name=habit_name,
+                                   description=description)
+
 
 def process_habit_goal_step(message, user_id, habit_name, description):
     goal = message.text
     msg = bot.send_message(message.chat.id, "Введите частоту напоминаний (например, 'ежедневно'):")
-    bot.register_next_step_handler(msg, process_habit_frequency_step, user_id=user_id, habit_name=habit_name, description=description, goal=goal)
+    bot.register_next_step_handler(msg, process_habit_frequency_step, user_id=user_id, habit_name=habit_name,
+                                   description=description, goal=goal)
+
 
 def process_habit_frequency_step(message, user_id, habit_name, description, goal):
     frequency = message.text
+    username = message.from_user.username
     # Добавляем привычку в базу и связываем её с пользователем
-    add_habit_to_user_list_directly(user_id, habit_name, description, goal, frequency)
+    add_habit_to_user_list_directly(username, user_id, habit_name, description, goal, frequency)
     bot.send_message(message.chat.id, "Привычка успешно добавлена в ваш список.")
 
 
 # Функция - обработка запроса на вызов функции show_all_habits() - СМ. СТРОКУ 89
 @bot.callback_query_handler(func=lambda call: call.data == 'all_habits')
-def add_new_habit(call):
+def show_all_habits(call):
     # Эта функция должна быть реализована для начала процесса добавления новой привычки
     # bot.send_message(call.message.chat.id, "Функция добавления новой привычки ещё не реализована.")
     username = call.from_user.username
@@ -112,14 +122,14 @@ def add_new_habit(call):
 
 # Функция - вывод всех привычек из таблицы 'habits' с возможностью выбора
 # новой привычки для добавления в список привычек пользователя - СМ. СТРОКУ 61
-@bot.message_handler(commands=['allhabits'])
-def show_all_habits(message):
-    habits = get_all_habits()
-    if not habits:
-        bot.send_message(message.chat.id, "Список привычек пуст.")
-        return
-    markup = generate_markup(habits, list_type='habits')
-    bot.send_message(message.chat.id, "Выберите привычку для добавления:", reply_markup=markup)
+# @bot.message_handler(commands=['allhabits'])
+# def show_all_habits(message):
+#     habits = get_all_habits()
+#     if not habits:
+#         bot.send_message(message.chat.id, "Список привычек пуст.")
+#         return
+#     markup = generate_markup(habits, list_type='habits')
+#     bot.send_message(message.chat.id, "Выберите привычку для добавления:", reply_markup=markup)
 
 
 # Функция - обработка запроса на добавление пользователю новой привычки из таблицы 'habits'
@@ -138,7 +148,8 @@ def handle_pagination(call):
     _, page, list_type = call.data.split('_')
     page = int(page)
     if list_type == 'habits':
-        habits = get_all_habits()
+        pass
+        # habits = get_all_habits()
     elif list_type == 'newhabits':
         habits = get_new_habits(call.from_user.username)
     markup = generate_markup(habits, page, list_type)
