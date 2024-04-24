fae65115d89508a58cbd3622eff70c189318ef4e
YZenia <164417240+YZenia@users.noreply.github.com>
2024-04-24 22:45:49 +0200

    ok

--- Telebot_UI.py
+++ Telebot_UI.py
@@ -7,7 +7,7 @@ from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 # Импорт функций из файла Python Logic_Back_end.py
 from Logic_Back_end import (add_or_get_user,
-                            add_habit_to_user_list, get_all_habits, get_user_habits, get_new_habits)
+                            add_habit_to_user_list, get_user_habits, get_new_habits, add_habit_to_user_list_directly, get_all_habits)
 
 # Ввод токена основного телеграм-бота и инициализация программы:
 # TOKEN = '6795112102:AAFBiEZg3Jgi2XxAoqsJvLzUGfSsmvNempo'
@@ -16,7 +16,7 @@ from Logic_Back_end import (add_or_get_user,
 # !!! ПЕРЕКЛЮЧИТЬ НА ОСНОВНОЙ ТЕЛЕГРАМ-БОТ В ФИНАЛЬНОЙ ВЕРСИИ ПРОГРАММЫ !!!
 
 # Ввод токена тестового телеграм-бота и инициализация программы
-TEST_TOKEN = '7088266760:AAG2r0Dz3GJAymtpxqrQpapNgVC91u8E23Q'
+TEST_TOKEN = '6795112102:AAFBiEZg3Jgi2XxAoqsJvLzUGfSsmvNempo'
 bot = telebot.TeleBot(TEST_TOKEN)
 
 
@@ -46,27 +46,53 @@ def send_welcome(message):
         return
 
     user_id = add_or_get_user(username)
-    welcome_text = "Добро пожаловать! Вот основные команды, которые вы можете использовать:"
+    welcome_text = "Добро пожаловать! Вот основные команды, которые вы можете использовать в боте Привычек:"
     markup = types.InlineKeyboardMarkup(row_width=2)
     commands_buttons = [
-        types.InlineKeyboardButton("Просмотр всех привычек", callback_data='all_habits'),
-        types.InlineKeyboardButton("Просмотр моих привычек", callback_data='list_habits'),
-        types.InlineKeyboardButton("Добавить новую привычку", callback_data='add_new_habit')
+        types.InlineKeyboardButton("Добавить свою П.", callback_data='add_new_habit'),
+        types.InlineKeyboardButton("Настройка моих П.", callback_data='list_habits'),
+        types.InlineKeyboardButton("База Привычек", callback_data='all_habits')
     ]
     markup.add(*commands_buttons)
     bot.send_message(message.chat.id, welcome_text, reply_markup=markup)
 
+# Функция по нажатию кнопки "База Привычек":
+# вывод всех привычек из таблицы 'habits', кроме уже добавленных в список привычек пользователя;
+# с возможностью выбора новой привычки для добавления в список привычек пользователя
+
+# @bot.callback_query_handler(func=lambda call: call.data == 'add_new_habit')
+# def show_all_habits_button(call):
+#     show_all_habits(call.message)
+@bot.callback_query_handler(func=lambda call: call.data == 'add_new_habit')
+def add_new_habit_button(call):
+    # Начало диалога для добавления новой привычки
+    msg = bot.send_message(call.message.chat.id, "Введите название новой привычки:")
+    bot.register_next_step_handler(msg, process_habit_name_step, user_id=call.from_user.id)
+
+def process_habit_name_step(message, user_id):
+    habit_name = message.text
+    msg = bot.send_message(message.chat.id, "Введите описание привычки:")
+    bot.register_next_step_handler(msg, process_habit_description_step, user_id=user_id, habit_name=habit_name)
+
+def process_habit_description_step(message, user_id, habit_name):
+    description = message.text
+    msg = bot.send_message(message.chat.id, "Введите цель привычки:")
+    bot.register_next_step_handler(msg, process_habit_goal_step, user_id=user_id, habit_name=habit_name, description=description)
+
+def process_habit_goal_step(message, user_id, habit_name, description):
+    goal = message.text
+    msg = bot.send_message(message.chat.id, "Введите частоту напоминаний (например, 'ежедневно'):")
+    bot.register_next_step_handler(msg, process_habit_frequency_step, user_id=user_id, habit_name=habit_name, description=description, goal=goal)
+
+def process_habit_frequency_step(message, user_id, habit_name, description, goal):
+    frequency = message.text
+    # Добавляем привычку в базу и связываем её с пользователем
+    add_habit_to_user_list_directly(user_id, habit_name, description, goal, frequency)
+    bot.send_message(message.chat.id, "Привычка успешно добавлена в ваш список.")
+
 
 # Функция - обработка запроса на вызов функции show_all_habits() - СМ. СТРОКУ 89
 @bot.callback_query_handler(func=lambda call: call.data == 'all_habits')
-def show_all_habits_button(call):
-    show_all_habits(call.message)
-
-
-# Функция по нажатию кнопки "Добавить новую привычку":
-# вывод всех привычек из таблицы 'habits', кроме уже добавленных в список привычек пользователя;
-# с возможностью выбора новой привычки для добавления в список привычек пользователя
-@bot.callback_query_handler(func=lambda call: call.data == 'add_new_habit')
 def add_new_habit(call):
     # Эта функция должна быть реализована для начала процесса добавления новой привычки
     # bot.send_message(call.message.chat.id, "Функция добавления новой привычки ещё не реализована.")
